d59cf3eb44283f4bb46e5770ddb40be031486a0e
quhairfoir <moragstc@gmail.com>
2018-08-06 13:57:00 -0400

    lowercase and reverse

--- callbacks/print_reverse.js
+++ callbacks/print_reverse.js
@@ -1,1 +1,14 @@
 var getHTML = require('./https-functions')
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step6/reverse.html'
+};
+
+function printReverse (html) {
+
+console.log(html.split("").reverse().join(""));
+
+}
+
+getHTML(requestOptions, printReverse);
